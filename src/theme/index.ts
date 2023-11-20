81f3553d187443df0922a27670c57cf1ba13bcaa
TonySilva7 <tony.deve7oper@gmail.com>
2023-11-20 17:38:25 -0300

    feat: config theme

---
+++ src/theme/index.ts
@@ -0,0 +1,29 @@
+export const theme = {
+  colors: {
+    blueDark: '#1E6F9F',
+    blue: '#4EA8DE',
+    purpleDark: '#5E60CE',
+    purple: '#8284FA',
+    gray700: '#0D0D0D',
+    gray600: '#1A1A1A',
+    gray500: '#262626',
+    gray400: '#333333',
+    gray300: '#808080',
+    gray200: '#d9d9d9',
+    gray100: '#f2f2f2',
+    danger: '#E25858',
+  },
+  fonts: {
+    family: 'Inter',
+    sizes: {
+      regular: 12,
+      medium: 14,
+      large: 16,
+    },
+    lineHeight: '140%',
+    weights: {
+      regular: 400,
+      bold: 700,
+    },
+  },
+}
